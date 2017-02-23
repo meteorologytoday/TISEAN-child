e4c9ec00d9de0f94f6ef4d44ad37a4ba73989d02
meteorologytoday <meteorologytoday@github.com>
2017-02-23 19:22:26 +0800

    setup class

---
+++ src/Prediction.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def findNearestNeighbor(center, data, n):
+		""" 
+		This function finds the nearest neighbor in <n>-dimensional Euclidean space equipped with usual metric.
+        <data> is a list of time series, each 
---
+++ src/TimeSeries.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+
+class TimeSeries:
+	def __init__(self, data):
+		self.data = np.array(data)
+
+
